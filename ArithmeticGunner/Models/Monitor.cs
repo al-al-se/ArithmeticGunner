686b58841a1b76872b15f34899ec293d6d9bd588
al-al-se <al.al.se@yandex.ru>
2021-06-06 12:42:28 +0300

    model 'Monitor' was upgraded

--- ArithmeticGunner/Models/Monitor.cs
+++ ArithmeticGunner/Models/Monitor.cs
@@ -21,9 +21,13 @@ namespace ArithmeticGunner.Models
     {
         string Arg1 {get;}
 
-        string CurrentOperation {get;}
+        State CurrentState {get;}
 
         string Arg2 {get;}
+
+        int Level {get;}
+
+        int Lives {get;}
 
         void StartGame();
 
@@ -70,12 +74,41 @@ namespace ArithmeticGunner.Models
 
         public void AcceptAnswer(string answer)
         {
+            if (CurrentState == State.TargetFound)
+            {
+                CurrentState = State.Shot;
+                System.Threading.Thread.Sleep(1000);
+                if (_operationHandler.AcceptAnswer(answer))
+                {
+                    CurrentState = State.TargetHit;
+                } else
+                {
+                    CurrentState = State.TargetFound;
+                }
+            }
+        }
 
+        private int __internalLevel = 1;
+
+        public int Level 
+        {
+            get {return __internalLevel;}
+            set {__internalLevel = value; OnPropertyChanged("Level");}
+        }
+
+        private int __internalLives = 10;
+
+        public int Lives
+        {
+            get {return __internalLives;}
+            set {__internalLives = value; OnPropertyChanged("Lives");}
         }
 
         public void StartGame()
         {
-
+            Lives = 10;
+            Level = 1;
+            CurrentState = State.TargetNotFound;
         }
 
     }
