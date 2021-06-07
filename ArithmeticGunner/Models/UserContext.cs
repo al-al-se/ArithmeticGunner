33f56e5e6c7ca3f750313a1f36ba9e3def997890
al-al-se <al.al.se@yandex.ru>
2021-06-07 10:22:15 +0300

    UserContext was done

--- ArithmeticGunner/Models/UserContext.cs
+++ ArithmeticGunner/Models/UserContext.cs
@@ -20,7 +20,19 @@ public class UserContext : DbContext, IUserRepository
 
     public void SaveResult(string login, int level)
     {
-
+        if (Users.Any(u => u.Login == login))
+        {
+            var u = Users.First(u => u.Login == login);
+            double f = (double)u.Count / (u.Count + 1);
+            u.Average = u.Average * f + level / (u.Count + 1);
+            u.Count++;
+            u.Best = Math.Max(u.Best,level);
+        } else
+        {
+            var u = new User() {Login = login, Best = level, Average = level, Count = 1};
+            Users.Add(u);
+        }
+        SaveChanges();
     }
 
     public IEnumerable<User> GetStatictics()
